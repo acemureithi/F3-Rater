47b7d1e0969142629da72c7c96fd37887bf01d52
Njuguna Mureithi <geofmureithi@users.noreply.github.com>
2020-06-10 15:39:00 +0300

    Update RatingsHelper.php

--- RatingsHelper.php
+++ RatingsHelper.php
@@ -3,8 +3,8 @@ class RatingsHelper extends \Prefab {
   static public function render($node) {
 	  $rating = $node['@attrib'];
 	  $rating['id'] = \Template::instance()->token($rating['id']);
-	  $x	= \Template::instance()->build('<div class="text-center"><input id="'.$rating['type'].'_rating_<?php echo '.$rating['id'].';?>" value="<?php echo $rating['value'];?>" class="ratings" /></div>');
-    return $x;
+	  $html = \Template::instance()->build('<div class="text-center"><input id="'.$rating['type'].'_rating_<?php echo '.$rating['id'].';?>" value="<?php echo $rating['value'];?>" class="ratings" /></div>');
+    return $html;
   }
   
 }
